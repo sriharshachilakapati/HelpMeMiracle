264fde9b1e55dab83cb0fe5a627a60bb0274a2e6
Sri Harsha Chilakapati <hello@goharsha.com>
2017-05-18 15:33:59 +0530

    Setup Express Template App

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+
+// Load the environment variables
+require('dotenv').config();
+
+let dbHost = process.env.DB_HOST;
+let dbPort = process.env.DB_PORT;
+let dbName = process.env.DB_NAME;
+
+let httpPort = process.env.port || 8080;
+
+// Connect to the DB first using mongoose
+mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, mongooseErr =>
+{
+    if (mongooseErr)
+        throw mongooseErr;
+
+    // Create an HTTP server using Express
+    let app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ "extended": true }));
+    app.use(express.static(path.join(__dirname, "public")));
+
+    app.get('/', (req, res) =>
+    {
+        res.send("Hello World!!!");
+    });
+
+    // Start the HTTP server
+    app.listen(httpPort, expressErr =>
+    {
+        if (expressErr)
+            throw expressErr;
+
+        console.log(`Server is running on port ${httpPort}`);
+    });
+});
