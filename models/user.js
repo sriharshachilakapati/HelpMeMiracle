12e84647e7ebe38ecd0785ee82aaa693364302f6
gayathri0897 <dondapati.gayathri97@gmail.com>
2017-05-18 17:07:27 +0530

    Added user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+let schema = mongoose.Schema({
+    "mid": {
+        "type": String,
+        "required": true,
+        "unique": true
+    },
+
+    "name": {
+        "type": String,
+        "required": true
+    },
+
+    "mail": {
+        "type": String,
+        "required": true
+    },
+
+    "password": {
+        "type": String,
+        "required": true
+    }
+});
