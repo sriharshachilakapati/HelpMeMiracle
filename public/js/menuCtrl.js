efda8d2cef005e0765a3282391b53736f7a780ae
Sri Harsha Chilakapati <hello@goharsha.com>
2017-05-19 19:20:22 +0530

    Change menus when logged in

---
+++ public/js/menuCtrl.js
@@ -0,0 +1,22 @@
+app.controller('menuCtrl', function($scope, $window)
+{
+    $scope.updateMenus = function()
+    {
+        let user = $window.localStorage.getItem('user');
+
+        if (user == null)
+            $scope.nav = [
+                { "name": "Login", "href": "/login" },
+                { "name": "Register", "href": "/register" }
+            ];
+        else
+            $scope.nav = [
+                { "name": "All Tickets", "href": "/" },
+                { "name": "My Tickets", "href": "/my" },
+                { "name": "Logout", "href": "/logout" }
+            ];
+    };
+
+    $scope.$on('$routeChangeSuccess', $scope.updateMenus);
+    $scope.updateMenus();
+});
