b13b8362b70bf5ae1054ca3cda6ae23e35a32b1b
bharat1235 <bharat.prabhas007@gmail.com>
2017-05-24 00:41:23 +0530

    Implemented all tickets view in home page

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,4 +1,20 @@
-app.controller('mainCtrl', function($scope)
+app.controller('mainCtrl', function($scope, $http)
 {
-    // TODO:
+    let request = {
+        "url": "/tickets/all",
+        "method": "GET"
+    };
+
+    $http(request)
+        .success(data =>
+        {
+            if (!data.success)
+                Materialize.toast(data.message, 4000);
+            else
+                $scope.tickets = data.tickets;
+        })
+        .catch(err =>
+        {
+            Materialize.toast("Network down, try again in few minutes", 4000);
+        });
 });
